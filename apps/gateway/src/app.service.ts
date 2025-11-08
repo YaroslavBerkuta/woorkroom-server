08d1dc2ff6c1dc935f824c369ea19716397231b1
YaroslavBerkuta <yaroslavberkuta@gmail.com>
2025-11-08 22:17:37 +0200

    add some libs

--- apps/gateway/src/app.service.ts
+++ apps/gateway/src/app.service.ts
@@ -1,7 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  private readonly logger = new Logger(AppService.name);
+  constructor() {
+    this.logger.debug('Hello World!', { message: 123});
+  }
   getHello(): string {
     return 'Hello World!';
   }
