99054cdd039ed0b8a540aa4b15069af35afd9289
YaroslavBerkuta <yaroslavberkuta@gmail.com>
2025-11-26 23:47:27 +0200

    config kuber

--- apps/authorization/src/main.ts
+++ apps/authorization/src/main.ts
@@ -1,15 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AuthorizationModule } from './authorization.module';
-import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice(AuthorizationModule, {
-    transport: Transport.RMQ,
-    options: {
-      urls: [process.env.RABBIT_URL],
-      queue: process.env.AUTHORIZATION_QUEUE,
-    },
-  });
+  const app = await NestFactory.createMicroservice(AuthorizationModule);
   await app.listen();
 }
-bootstrap();
+void bootstrap();
--- apps/companys/src/main.ts
+++ apps/companys/src/main.ts
@@ -1,15 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { CompanysModule } from './companys.module';
-import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice(CompanysModule, {
-    transport: Transport.RMQ,
-    options: {
-      urls: [process.env.RABBIT_URL],
-      queue: process.env.COMPANYS_QUEUE,
-    },
-  });
+  const app = await NestFactory.createMicroservice(CompanysModule);
   await app.listen();
 }
-bootstrap();
+void bootstrap();
